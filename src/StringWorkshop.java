a8e2f3309c53017f89d211bc107e279298fff268
Dylan Monti di Sopra <kesto21@gmail.com>
2024-02-28 16:43:02 -0700

    Hello World - Setup

---
+++ src/StringWorkshop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringWorkshop {
+
+    static Scanner in;
+
+    public static void main(String[] args) {
+
+        in = new Scanner (System.in);
+
+        System.out.println("What ");
+        String str = in.next();
+        in.next(); // clearing the input stream
+
+        System.out.println("Enter a number for one of the following:\n: 1 - Add more contene");
+
+    }
+}
